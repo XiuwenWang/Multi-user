c5248c1b60feff63bb8c6aa7834fc8a288a4f6aa
xiumiing@gmail.com <13872829574@163.com>
2018-10-09 03:20:23 +0800

    ...

--- app/src/main/java/com/xiumiing/user/MainActivity.java
+++ app/src/main/java/com/xiumiing/user/MainActivity.java
@@ -1,29 +1,26 @@
 package com.xiumiing.user;
 
 import android.content.Context;
-import android.os.Build;
+import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.blankj.utilcode.util.ReflectUtils;
 import com.blankj.utilcode.util.Utils;
 
-import java.lang.reflect.Field;
-
 public class MainActivity extends AppCompatActivity {
 
-    private final static String TAG = MainActivity.class.getSimpleName();
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Utils.init(this);
-        openFlymeParallelSpace();
+
     }
 
     int FLAG_MANAGED_PROFILE = 0x00000020; // 创建影子用户必要的flag
@@ -32,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
     private Object mFlymeParallelSpaceUserInfo = null; // multi-open UserInfo
 
     public void openFlymeParallelSpace() {
-        mUserManager = (UserManager) getSystemService(Context.USER_SERVICE);
 
         UserHandle userHandle = android.os.Process.myUserHandle();
         int getIdentifier = ReflectUtils.reflect(userHandle).method("getIdentifier").get();
+        Log.d(TAG, "getIdentifier = " + getIdentifier);
+
+        mUserManager = (UserManager) getSystemService(Context.USER_SERVICE);
+        Log.d(TAG, "mUserManager = "+mUserManager.getUserName());
         mFlymeParallelSpaceUserInfo = ReflectUtils
                 .reflect(mUserManager)
                 .method("createProfileForUser",
@@ -43,8 +43,23 @@ public class MainActivity extends AppCompatActivity {
                         FLAG_MANAGED_PROFILE,
                         getIdentifier);
         int userId = ReflectUtils.reflect(mFlymeParallelSpaceUserInfo).field("id").get();
+        Log.d(TAG, "userId = " + userId);
+
         Object iActivityManager = ReflectUtils.reflect("android.app.ActivityManagerNative").method("getDefault").get();
+        Log.d(TAG, "iActivityManager = " + iActivityManager);
+
         Boolean isOk = ReflectUtils.reflect(iActivityManager).method("startUserInBackground", userId).get();
+
         Log.d(TAG, "startUserInBackground() userId = " + userId + " | isOk = " + isOk);
+    }
+
+    public void onClick(View view) {
+        try {
+        openFlymeParallelSpace();
+        } catch (Exception e) {
+//            LogUtils.e(e);
+        }
+
+
     }
 }
